1f0a93abf18e9c4be5b522bd36610f07bb814a19
ParkEunBlank <47646799+ParkEunBlank@users.noreply.github.com>
2020-07-25 23:57:38 +0200

    init

---
+++ BotMakerExceptions.py
@@ -0,0 +1,28 @@
+
+
+class GuildIdNotFound(ValueError):
+    pass
+
+
+class GuildNameNotFound(ValueError):
+    pass
+
+
+class RoleNameNotFound(ValueError):
+    pass
+
+
+class ChannelNotSet(ValueError):
+    pass
+
+
+class RoleIdNotFound(ValueError):
+    pass
+
+
+class MemberNameNotFound(ValueError):
+    pass
+
+
+class MemberIdNotFound(ValueError):
+    pass
---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import discord
+from discord.ext import commands
+import os
+import configparser
+import ctypes
+os.chdir(os.path.dirname(__file__))
+#cogs = ["cogs.twitterCog", "cogs.twitchCog", "cogs.botowner", "cogs.gspreadCog", "cogs.tw", "cogs.twitchCog"]
+
+cogs = ["first"]
+TOKEN = "NzM2NDM4NjY5NTUxOTkyOTM2.Xxuz9Q.bqK2zosQXpEewxrvlwWOrgTFl8k"
+
+client = commands.Bot(command_prefix = ".", owner_ids = (181125548389433344 , 97502897012490240))
+ctypes.windll.kernel32.SetConsoleTitleA("BotMaker")
+
+if __name__ == "__main__":
+    for ex in cogs:
+        try:
+            client.load_extension(ex)
+            print(f"Loaded {ex}")
+        except Exception as error:
+            print(error, error.__class__)
+    print(client.extensions)
+    client.run(TOKEN)
